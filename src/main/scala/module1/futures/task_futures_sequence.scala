557adb0e44cc75ffca93f5649236b9e07041b607
Gadzhidiran Alexander <alesandro.erg@gmail.com>
2023-05-14 15:27:37 +0300

    Scala Future homework

--- src/main/scala/module1/futures/task_futures_sequence.scala
+++ src/main/scala/module1/futures/task_futures_sequence.scala
@@ -1,18 +1,16 @@
 package module1.futures
 
-import HomeworksUtils.TaskSyntax
-
 import scala.concurrent.{ExecutionContext, Future}
 
 object task_futures_sequence {
 
   /**
    * В данном задании Вам предлагается реализовать функцию fullSequence,
-   * похожую на Future.sequence, но в отличии от нее,
-   * возвращающую все успешные и не успешные результаты.
-   * Возвращаемое тип функции - кортеж из двух списков,
+   * похожую на Future.sequence, но в отличие от нее,
+   * возвращающую все успешные и неуспешные результаты.
+   * Возвращаемый тип функции - кортеж из двух списков,
    * в левом хранятся результаты успешных выполнений,
-   * в правово результаты неуспешных выполнений.
+   * в правом результаты неуспешных выполнений.
    * Не допускается использование методов объекта Await и мутабельных переменных var
    */
   /**
@@ -20,6 +18,13 @@ object task_futures_sequence {
    * @return асинхронную задачу с кортежом из двух списков
    */
   def fullSequence[A](futures: List[Future[A]])
-                     (implicit ex: ExecutionContext): Future[(List[A], List[Throwable])] =
-    task"Реализуйте метод `fullSequence`"()
+                     (implicit ex: ExecutionContext): Future[(List[A], List[Throwable])] = {
+    futures.foldRight(Future((List.empty[A], List.empty[Throwable]))) {
+      (fa, fr) =>
+          fa.flatMap(res =>
+            fr.map(agg => (res :: agg._1, agg._2))
+          ).recoverWith(e =>
+            fr.map(agg => (agg._1, e :: agg._2)))
+    }
+  }
 }
